4722984a17b6caf73f2c89ff4d8277ad2565b222
Jefferson Siqueira Balivo <balivo@outlook.com.br>
2017-06-28 14:54:50 -0300

    Backend (and Backend.Tests) project creation and NuGet Packages updates

---
+++ XamarinSampleSolution/XamarinSample.Backend/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace XamarinSample.Backend.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ XamarinSampleSolution/XamarinSample.Backend/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace XamarinSample.Backend.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
